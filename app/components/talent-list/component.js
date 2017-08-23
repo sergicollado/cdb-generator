3071877bb5152c4d1684de9e5f55e0bd23a51c1f
sergicollado <sergicollado@gmail.com>
2017-08-24 00:27:28 +0200

    add remove talent

--- app/components/talent-list/component.js
+++ app/components/talent-list/component.js
@@ -1,21 +1,32 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-
   init: function(){
     this._super(...arguments);
     let self = this;
     Ember.$.getJSON("talents.json").then(function(data){
-      self.set('dataTalents',data);
+      let talents = data.map(function(obj){
+        return Ember.Object.create(obj);
+      });
+      self.set('dataTalents',talents);
     })
   },
   store: Ember.inject.service(),
+  talentService: Ember.inject.service('talents'),
   isShowingTalentSelector:false,
   actions: {
     toogleDataTalents() {
       this.toggleProperty('isShowingTalentSelector');
     },
+    removeTalent(talent){
+      this.get('character.talents').removeObject(talent);
+      this.get('dataTalents').findBy('name',talent.get('name')).set('isSelected',false);
+    },
     setTalent(talent){
+      if(talent.get('isSelected')){
+        return;
+      }
+      talent.set('isSelected',true);
       let store = this.get('store');
       this.get('character.talents').addObject(store.createRecord('talent',{
           name: talent.name,
--- app/services/talents.js
+++ app/services/talents.js
@@ -1,5 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Service.extend({
-
+    TRAINNING:'TRAINNING',
+    MORE_THAN_3:'MORE_THAN_3',
+    KNOWLEDGE_COMPUTER:'KNOWLEDGE_COMPUTER',
+    MORE_THAN_3_TRAINNING:'MORE_THAN_3_TRAINNING',
+    AND:'AND',
+    OR: 'OR'
 });
